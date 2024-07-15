fb7f13182848c4d3ae79e17db11050b7ad29e086
Lupus <163419314+LupusJM@users.noreply.github.com>
2024-07-15 12:42:37 +0200

    Update main.py
    Debugged post downloads -> click on the stories tab

--- main.py
+++ main.py
@@ -9,36 +9,40 @@ import time
 
 def get_instagram_stories(username, download_folder):
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
 
     driver.get("https://fastdl.app/")
 
     try:
-        print("Waiting for cookies button..")
+        # Cookies consent
+        print("Waiting for cookies button...")
         cookies_button = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH, "//button[@aria-label='Consent']"))
         )
         cookies_button.click()
         print("Cookies button clicked.")
 
-        print("Waiting for username input..")
+        # Username input
+        print("Waiting for username input...")
         url_input = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH, "//input[@id='search-form-input']"))
         )
         url_input.send_keys(f"{username}")
         print("Username entered.")
 
-        print("Waiting for download button..")
+        # Download button
+        print("Waiting for download button...")
         download_button = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH, "//button[@class='search-form__button']"))
         )
         download_button.click()
         print("Download button clicked.")
 
+        # Remove popup ad if it appears
         try:
-            print("Removing popup ad..")
+            print("Removing popup ad...")
             popup_element = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "ads-modal"))
             )
@@ -50,6 +54,21 @@ def get_instagram_stories(username, download_folder):
         except Exception as e:
             print("No popup ad detected or an error occurred:", e)
 
+        time.sleep(6)
+
+        # Click the "Stories" tab
+        try:
+            print("Waiting for 'Stories' tab...")
+            stories_tab = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, "//li[@class='tabs-component__item']/button[contains(text(), 'stories')]"))
+            )
+            stories_tab.click()
+            print("'Stories' tab clicked.")
+        except Exception as e:
+            print("An error occurred while trying to click the 'Stories' tab:", e)
+            return False
+
+        # Click "See more" buttons until they no longer appear
         while True:
             try:
                 see_more_button = WebDriverWait(driver, 10).until(
@@ -57,17 +76,19 @@ def get_instagram_stories(username, download_folder):
                 )
                 see_more_button.click()
                 print("See more button clicked.")
-                time.sleep(2)
+                time.sleep(2)  # Wait for new content to load
             except:
                 print("No more 'See more' buttons.")
                 break
 
-        print("Waiting for download buttons to become clickable..")
+        # Wait for download buttons to become clickable
+        print("Waiting for download buttons to become clickable...")
         download_buttons = WebDriverWait(driver, 20).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a.button--filled"))
         )
         print(f"Found {len(download_buttons)} download buttons.")
 
+        # Downloading stories
         for index, download_button in enumerate(download_buttons):
             download_url = download_button.get_attribute("href")
             response = requests.head(download_url)
@@ -85,7 +106,7 @@ def get_instagram_stories(username, download_folder):
             filename = os.path.join(download_folder, "Stories", f"LupusDownloader_{username}_{download_date}_{index + 1}.{extension}")
 
             if not is_already_downloaded(download_url, download_folder):
-                print(f"Downloading {download_url} to {filename}..")
+                print(f"Downloading {download_url} to {filename}...")
                 response = requests.get(download_url)
                 with open(filename, 'wb') as file:
                     file.write(response.content)
@@ -128,7 +149,7 @@ def run_script():
     if not os.path.exists(stories_folder):
         os.makedirs(stories_folder)
 
-    downloaded_links_file = os.path.join(download_folder, "downloaded_links.txt")
+    downloaded_links_file = os.path.    join(download_folder, "downloaded_links.txt")
     if not os.path.exists(downloaded_links_file):
         with open(downloaded_links_file, "w"):
             pass
